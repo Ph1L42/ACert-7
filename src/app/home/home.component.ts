115add220900e5e336ee0af627b1be3e35faf169
pphan <philipp.phan@adesso.de>
2024-11-05 14:50:59 +0100

    05112024

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,18 +4,21 @@ import {MovieItemComponent} from '../movie-item/movie-item.component';
 import {Movie} from '../model/movie.model';
 import {MoviesService} from '../services/movies.service';
 import {FavoritesService} from '../services/favorites.service';
+import {Observable} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-    imports: [
-        HighlightDirective,
-        MovieItemComponent
-    ],
+  imports: [
+    HighlightDirective,
+    MovieItemComponent,
+    AsyncPipe
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  protected movies: Signal<Movie[]> = inject(MoviesService).getMovies();
+  protected movies$: Observable<Movie[]> = inject(MoviesService).getMovies();
   protected favoritesService = inject(FavoritesService);
 }
